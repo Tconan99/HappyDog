a9ff3a7e4f4d573f7dc6d39889af35043816ded9
erpengxue <erpengxue@tencent.com>
2021-02-27 21:21:56 +0800

    style: format

--- app/src/main/java/com/example/androiddevchallenge/ui/DetailView.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/DetailView.kt
@@ -1,9 +1,29 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProvideTextStyle
@@ -25,7 +45,7 @@ fun DetailView(dog: DogEntity, onAdoptClick: (DogEntity) -> Unit = {}) {
         Modifier
             .fillMaxSize()
             .background(MaterialTheme.colors.background)
-            .clickable {  }
+            .clickable { }
     ) {
         Box {
             Image(
--- app/src/main/java/com/example/androiddevchallenge/ui/ListView.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/ListView.kt
@@ -1,9 +1,23 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -23,10 +37,12 @@ fun ListView(dogs: List<DogEntity>, onDogClick: (DogEntity) -> Unit = {}) {
         verticalArrangement = Arrangement.spacedBy(0.5.dp)
     ) {
         items(dogs) { dog ->
-            ItemView(dog = dog,
+            ItemView(
+                dog = dog,
                 modifier = Modifier
                     .clickable { onDogClick(dog) }
-                    .fillMaxWidth())
+                    .fillMaxWidth()
+            )
         }
     }
 }
